9748e4c89997a49ad8a14b8af3eaf6eca39cad67
Nathaniel Dessert <nhdlite@Nathaniels-MacBook-Pro.local>
2019-01-24 23:30:45 -0800

    Added a convert to roman numeral method with tests

---
+++ romanNumerals.js
@@ -0,0 +1,57 @@
+function convertToRomanNumeral(number) {
+    const romanNumeralConversion = [
+        { romC: 'I', romI: 1 },
+        { romC: 'V', romI: 5 },
+        { romC: 'X', romI: 10 },
+        { romC: 'L', romI: 50 },
+        { romC: 'C', romI: 100 },
+        { romC: 'D', romI: 500 },
+        { romC: 'M', romI: 1000 },
+    ];
+
+    let tensPlaceIndex = 0;
+    let convertedDigits = []; // Entries will be added in reverse. Will need to flip them before returning.
+
+    if (number <= 0) {
+        throw "Not a valid number";
+    }
+
+    while(number > 0) {
+        const currentDigit = number % 10;
+        let convertedValue = '';
+
+        if (currentDigit > 0 && currentDigit < 4) {
+            for (let i = 0; i < currentDigit; i++){
+                convertedValue = convertedValue + romanNumeralConversion[tensPlaceIndex].romC;
+            }
+        } else if (currentDigit === 4) {
+            if (tensPlaceIndex + 1 > romanNumeralConversion.length) {
+                throw "Roman numerals cannot be greater than 3999";
+            }
+            convertedValue = romanNumeralConversion[tensPlaceIndex].romC + romanNumeralConversion[tensPlaceIndex + 1].romC;
+        } else if (currentDigit > 4 && currentDigit < 9) {
+            convertedValue = romanNumeralConversion[tensPlaceIndex + 1].romC;
+            for (let i = 0; i < currentDigit - 5; i++){
+                convertedValue = convertedValue + romanNumeralConversion[tensPlaceIndex].romC;
+            }
+        } else if (currentDigit === 9) {
+            if (tensPlaceIndex + 2 > romanNumeralConversion.length) {
+                throw "Roman numerals cannot be greater than 3999";
+            }
+            convertedValue = romanNumeralConversion[tensPlaceIndex].romC + romanNumeralConversion[tensPlaceIndex + 2].romC;
+        } else {
+            // Do Nothing. The digit is a 0.
+        }
+
+        convertedDigits.push(convertedValue);
+
+        number = Math.floor(number / 10);
+        tensPlaceIndex = tensPlaceIndex + 2;
+    }
+
+    return convertedDigits.reverse().join('');
+};
+
+module.exports = {
+    convertToRomanNumeral
+};
---
+++ romanNumerals.test.js
@@ -0,0 +1,99 @@
+const test = require('tape');
+const romanNumerals = require('./romanNumerals');
+
+test('1 yields I', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(1);
+    t.equals(result, 'I');
+    t.end();
+});
+
+test('3 yields III', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(3);
+    t.equals(result, 'III');
+    t.end();
+});
+
+test('4 yields IV', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(4);
+    t.equals(result, 'IV');
+    t.end();
+});
+
+test('7 yields VII', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(7);
+    t.equals(result, 'VII');
+    t.end();
+});
+
+test('9 yields IX', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(9);
+    t.equals(result, 'IX');
+    t.end();
+});
+
+test('10 yields X', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(10);
+    t.equals(result, 'X');
+    t.end();
+});
+
+test('13 yields XIII', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(13);
+    t.equals(result, 'XIII');
+    t.end();
+});
+
+test('14 yields XIV', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(14);
+    t.equals(result, 'XIV');
+    t.end();
+});
+
+test('19 yields XIX', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(19);
+    t.equals(result, 'XIX');
+    t.end();
+});
+
+test('33 yields XXXIII', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(33);
+    t.equals(result, 'XXXIII');
+    t.end();
+});
+
+test('40 yields XL', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(40);
+    t.equals(result, 'XL');
+    t.end();
+});
+
+test('49 yields XL', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(49);
+    t.equals(result, 'XLIX');
+    t.end();
+});
+
+test('50 yields L', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(50);
+    t.equals(result, 'L');
+    t.end();
+});
+
+test('99 yields XCIX', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(99);
+    t.equals(result, 'XCIX');
+    t.end();
+});
+
+test('246 yields CCXLVI', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(246);
+    t.equals(result, 'CCXLVI');
+    t.end();
+});
+
+
+test('3999 yields MMMCMXCIX', (t) => {
+    const result = romanNumerals.convertToRomanNumeral(3999);
+    t.equals(result, 'MMMCMXCIX');
+    t.end();
+});
